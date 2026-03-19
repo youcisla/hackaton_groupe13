722c596fdacf5aa6abd0b631565f5c2272d23227
Youcisla <chehbouby@yahoo.fr>
2026-03-19 11:07:24 +0100

    img

--- frontend/src/api/datalake.js
+++ frontend/src/api/datalake.js
@@ -1,21 +1,13 @@
-import { API_BASE } from './config.js'
+import api from './config.js'
 
 export async function getDataLakeStats(batchId) {
-  const url = batchId
-    ? `${API_BASE}/datalake/stats?batchId=${batchId}`
-    : `${API_BASE}/datalake/stats`
-  const res = await fetch(url)
-  const json = await res.json()
-  if (!json.success) throw new Error(json.error)
-  return json.data
+  const params = batchId ? { batchId } : {}
+  const res = await api.get('/datalake/stats', { params })
+  return res.data.data
 }
 
 export async function getDataLakeZone(zone, batchId) {
-  const url = batchId
-    ? `${API_BASE}/datalake/${zone}?batchId=${batchId}`
-    : `${API_BASE}/datalake/${zone}`
-  const res = await fetch(url)
-  const json = await res.json()
-  if (!json.success) throw new Error(json.error)
-  return json
+  const params = batchId ? { batchId } : {}
+  const res = await api.get(`/datalake/${zone}`, { params })
+  return res.data
 }
